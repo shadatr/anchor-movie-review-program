96b91e09b0bf2b9e854a4c2a9c489c000add84ef
shdab <130516923+shadatr@users.noreply.github.com>
2024-05-05 22:01:10 +0300

    first commit

---
+++ tests/anchor-movie-review-program.ts
@@ -0,0 +1,30 @@
+import * as anchor from "@coral-xyz/anchor"
+import { Program } from "@coral-xyz/anchor"
+import { expect } from "chai"
+import { AnchorMovieReviewProgram } from "../target/types/anchor_movie_review_program"
+
+describe("anchor-movie-review-program", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
+
+  const program = anchor.workspace
+    .AnchorMovieReviewProgram as Program<AnchorMovieReviewProgram>
+
+  const movie = {
+    title: "Just a test movie",
+    description: "Wow what a good movie it was real great",
+    rating: 5,
+  }
+
+  const [moviePda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from(movie.title), provider.wallet.publicKey.toBuffer()],
+    program.programId
+  )
+
+  it("Movie review is added`", async () => {})
+
+  it("Movie review is updated`", async () => {})
+
+  it("Deletes a movie review", async () => {})
+})
